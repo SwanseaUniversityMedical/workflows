9c65199d3b1ff447dfd4b8aaada460476610d04d
Rowan Dash <rowan.dash@swansea.ac.uk>
2024-05-30 11:23:33 +0100

    feat: optimize title commitlint to use bun instead of npm for speed (#157)

--- .github/commitlint.config.mjs
+++ .github/commitlint.config.mjs
@@ -1,10 +1,10 @@
-import { RuleConfigSeverity } from '@commitlint/types';
+// import { RuleConfigSeverity } from '@commitlint/types';
 
 export default {
   extends: ['@commitlint/config-conventional'],
   parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
-    'scope-enum': [RuleConfigSeverity.Error, 'always', [
+    'scope-enum': [2, 'always', [
         '',
         'deps',
         'containers',

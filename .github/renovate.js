3de8ebaaa0ec1b60f0d26e63f74ddd29656899eb
Joss Whittle <joss@chi.swan.ac.uk>
2025-03-05 13:50:25 +0000

    fix: renovate schedule

--- .github/renovate.js
+++ .github/renovate.js
@@ -25,9 +25,8 @@ module.exports = {
   // requests for dependencies unless they override the schedule.
   updateNotScheduled: false,
   timezone: "Europe/London",
-  //after 10pm and before 5am every weekday
   schedule: [
-    "* 22-23,0-4 * * 1-5"
+    "0 * * * *"
   ],
 
   // This setting helps handle breaking changes to Renovate bot when its version changes.
